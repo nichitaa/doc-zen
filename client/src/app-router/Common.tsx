3cc31e6089473a85a50c2a78fa9f72d005aa8c6b
Pasecinic Nichita <nichitastrix@gmail.com>
2022-01-04 22:11:49 +0200

    using my own auh-react-router lib

--- client/src/app-router/Common.tsx
+++
@@ -1,6 +0,0 @@
-import { ReactElement, FC, Suspense } from 'react';
-import Loading from '@components/shared/Loading/Loading';
-
-export const Common: FC<{ component: ReactElement }> = ({
-  component,
-}): ReactElement => <Suspense fallback={<Loading />}>{component}</Suspense>;
---
+++ client/src/routes.tsx
@@ -0,0 +1,56 @@
+import {IRoutesConfig} from "auth-react-router";
+import React, {lazy} from "react";
+import {Navigate} from "react-router-dom";
+import Loading from "@components/shared/Loading/Loading";
+
+// public
+const HomePage = lazy(() => import('@pages/Home/HomePage'));
+
+// private
+const UploadDocumentPage = lazy(
+  () => import('@pages/DocumentUpload/DocumentUploadPage')
+);
+const ProfilePage = lazy(() => import('@pages/Profile/ProfilePage'));
+
+// common
+const SharedDocumentsPage = lazy(
+  () => import('@pages/SharedDocuments/SharedDocumentsPage')
+);
+const NotFoundPage = lazy(() => import('@pages/NotFound/NotFoundPage'));
+
+
+export const routes: IRoutesConfig = {
+  publicRedirectRoute: '/home',
+  privateRedirectRoute: '/profile',
+  defaultFallback: <Loading/>,
+  public: [
+    {
+      path: '/home',
+      component: <HomePage/>,
+    },
+  ],
+  private: [
+    {
+      path: '/upload',
+      component: <UploadDocumentPage/>,
+    },
+    {
+      path: '/profile',
+      component: <ProfilePage/>,
+    },
+  ],
+  common: [
+    {
+      path: '/',
+      component: <Navigate to={'/home'}/>, // redirect to home
+    },
+    {
+      path: '/shared',
+      component: <SharedDocumentsPage/>,
+    },
+    {
+      path: '*',
+      component: <NotFoundPage/>,
+    },
+  ],
+}
