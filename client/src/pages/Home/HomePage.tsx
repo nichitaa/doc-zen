14ffa34f3b461bf0f0f4e0dbf10bc66a864829a9
Pasecinic Nichita <nichittaa@gmail.com>
2022-01-14 10:21:28 +0200

    added home page description

--- client/src/pages/Home/HomePage.tsx
+++ client/src/pages/Home/HomePage.tsx
@@ -56,23 +56,10 @@ const HomePage = () => {
         <Col span={12}>
           <Row>
             <Paragraph>
-              Lorem Ipsum is simply dummy text of the printing and typesetting
-              industry. Lorem Ipsum has been the industry's standard dummy text
-              ever since the 1500s, when an unknown printer took a galley of
-              type and scrambled it to make a type specimen book. It has
-              survived not only five centuries, but also the leap into
-              electronic typesetting, remaining essentially unchanged. It was
-              popularised in the 1960s with the release of Letraset sheets
-              containing Lorem Ipsum passages, and more recently with desktop
-              publishing software like Aldus PageMaker including versions of
-              Lorem Ipsum.
-              <Text strong>
-                Lorem Ipsum is simply dummy text of the printing and typesetting
-                industry. Lorem Ipsum has been the industry's standard dummy
-                text ever since the 1500s, when an unknown printer took a galley
-                of type and scrambled it to make a type specimen book.
-              </Text>
-              .
+              <Text strong={true}>
+                Doc-Zen
+              </Text> is a secure cloud storage application that stores and encrypts your documents. You can choose what
+              level of security to apply on your documents and share them safely through a unique ID
             </Paragraph>
           </Row>
           <Divider />
